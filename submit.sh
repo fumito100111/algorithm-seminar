e9c685a7ab73cc7e899b06693b247dc27b503d6d
fumito100111 <146402169+fumito100111@users.noreply.github.com>
2025-05-19 17:03:49 +0900

    Add git pull

--- submit.sh
+++ submit.sh
@@ -2,6 +2,7 @@
 
 make test DATE=$(date "+%Y%m%d")
 git branch -M main
+git pull origin main
 git add $(date "+%Y%m%d")/$(git config user.name)
 git commit -m "Submitted from $(git config user.name) ($(date '+%Y-%m-%d %H:%M:%S'))"
 git push -u origin main
