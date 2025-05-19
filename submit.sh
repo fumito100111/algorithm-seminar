59f8068fb39b50535e2c69bdef2d8b60c31caa93
fumito100111 <146402169+fumito100111@users.noreply.github.com>
2025-05-19 16:26:12 +0900

    Create submit.sh

---
+++ submit.sh
@@ -0,0 +1,8 @@
+#!bin/sh
+
+make test DATE=$(date "+%Y%m%d")
+git branch -M main
+git add $(date "+%Y%m%d")/$(git config user.name)
+git commit -m "Submitted from $(git config user.name) ($(date '+%Y-%m-%d %H:%M:%S'))"
+git push -u origin main
+echo "提出完了しました."
