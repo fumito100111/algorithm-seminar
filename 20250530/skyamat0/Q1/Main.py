0ff2d11fa123c672dba674d6f5653df0f5fe4471
Yamato Sakurai <yamatosakurai@Yamatos-MacBook-Air.local>
2025-05-30 13:57:21 +0900

    Algorithm-Seminar-2025-05-30 ()

--- 20250530/skyamat0/Q1/Main.py
+++ 20250530/skyamat0/Q1/Main.py
@@ -1,6 +1,9 @@
 def main():
-    print("Hello World (Python)")
-    pass
+    N = int(input())
+    for i in range(N):
+        l = list(map(int, input().split()))
+        print(l[0] + l[1])
+    return 0
 
 if __name__ == '__main__':
     main()
--- 20250530/skyamat0/Q2/Main.py
+++ 20250530/skyamat0/Q2/Main.py
@@ -1,6 +1,13 @@
+import heapq
+
 def main():
-    print("Hello World (Python)")
-    pass
+    N, K = list(map(int, input().split()))
+    topk_players = heapq.heapify()
+    for i in range(K):
+        heapq.heappush(topk_players, input())
+    for j in range(K):
+        print(heapq.heappop(topk_players))
+    return 0
 
 if __name__ == '__main__':
     main()
