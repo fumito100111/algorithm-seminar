954153f3a66a64e7518a82578d38b91607986c32
kimotu4632uz <32472396+kimotu4632uz@users.noreply.github.com>
2025-06-06 14:44:33 +0900

    Algorithm-Seminar-2025-06-06 (kimotu4632uz)

--- 20250606/kimotu4632uz/Q1/Main.py
+++ 20250606/kimotu4632uz/Q1/Main.py
@@ -1,6 +1,15 @@
 def main():
-    print("Hello World (Python)")
-    pass
+    x, n = map(int, input().split())
+
+    result = 1
+    for i in reversed(bin(n)[2:]):
+        if i == '1':
+            result *= x
+            result %= 10000000
+        x *= x
+        x %= 10000000
+    print(result)
+
 
 if __name__ == '__main__':
     main()
