eb9d1cf60e55f20a59440fb594190aa966159e9e
Seiya-hub2003 <seiya@yamamotoseiyanoMacBook-Air.local>
2025-06-06 14:45:01 +0900

    Algorithm-Seminar-2025-06-06 (Seiya-hub2003)

--- 20250606/Seiya-hub2003/Q1/Main.py
+++ 20250606/Seiya-hub2003/Q1/Main.py
@@ -1,6 +1,15 @@
 def main():
-    print("Hello World (Python)")
-    pass
+    import math
+    x, n = list(map(int, input().split()))
+    result = 1
+    k = math.log(n, 2)+1
+    print(k)
+    for i in range(int(k)):
+        if (n>>i) & 0b1== 1:
+            result *= x
+        x *= x
+    print(result % 10000000)
+
 
 if __name__ == '__main__':
     main()
