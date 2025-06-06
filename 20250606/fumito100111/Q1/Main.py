15d13fa03305f86b09e35d1f74cf3c038a833dd5
fumito100111 <146402169+fumito100111@users.noreply.github.com>
2025-06-06 14:30:41 +0900

    Algorithm-Seminar-2025-06-06 (fumito100111)

--- 20250606/fumito100111/Q1/Main.py
+++ 20250606/fumito100111/Q1/Main.py
@@ -1,6 +1,14 @@
 def main():
-    print("Hello World (Python)")
-    pass
+    NUM = 10000000
+    x, n = map(int, input().split())
+    bin_n = bin(n)[2:]
+    result = 1
+    for i in bin_n[::-1]:
+        if i == '1':
+            result = (result * (x % NUM)) % NUM
+        x *= x
+    print(result)
 
+ 
 if __name__ == '__main__':
     main()
