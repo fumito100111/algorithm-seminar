97deb8279e5dcda44421a205cc80204f8ea4ff3b
Seiya-hub2003 <seiya@yamamotoseiyanoMacBook-Air.local>
2025-06-27 14:03:46 +0900

    Algorithm-Seminar-2025-06-27 (Seiya-hub2003)

--- 20250627/Seiya-hub2003/Q1/Main.py
+++ 20250627/Seiya-hub2003/Q1/Main.py
@@ -1,5 +1,19 @@
 def main():
-    print("Hello World (Python)")
+    R, C = list(map(int, input().split()))
+
+    str = "black"
+    li = [1]*15
+    if R >= 9:
+        R = 16 - R
+    for i in range(int(R)):
+        for j in range(i,15-i):
+            li[j] *= -1
+    if li[C-1] == 1:
+        str = "white"
+    print(str)
+    
+
+
     pass
 
 if __name__ == '__main__':
--- 20250627/Seiya-hub2003/Q2/Main.py
+++ 20250627/Seiya-hub2003/Q2/Main.py
@@ -1,5 +1,13 @@
 def main():
-    print("Hello World (Python)")
+    N, K = list(map(int, input().split()))
+    A_k = list(map(int, input().split()))
+    X=[]
+    Y=[]
+    for i in range(N):
+        x, y = list(map(int, input().split()))
+        X.append(x)
+        Y.append(y)
+    print(X)
     pass
 
 if __name__ == '__main__':
